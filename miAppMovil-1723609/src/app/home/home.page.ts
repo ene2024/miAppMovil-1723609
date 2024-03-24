91dee4d2a3935ab6057525516ce0f4aaf8289209
Max Rada <xam900mr@gmail.com>
2024-03-23 20:46:02 -0600

    Ciclo de vidade un componente

--- miAppMovil-1723609/src/app/home/home.page.ts
+++ miAppMovil-1723609/src/app/home/home.page.ts
@@ -7,7 +7,41 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+    constructor() {
+      console.log("AppComponent: Constructor");
+    }
+    ngOnChanges(){
+      console.log("AppComponent: OnChanges");
+    }
+   
+    ngOnInit(){
+      console.log("AppComponent: OnInit");
+    }
+   
+    ngDoCheck(){
+      console.log("AppComponent: DoCheck");
+    }
+   
+    ngAfterContentInit(){
+      console.log("AppComponent: AfterContentInit");
+    }
+   
+    ngAfterContentChecked(){
+      console.log("AppComponent: AfterContentChecked");
+    }
+   
+    ngAfterViewInit(){
+      console.log("AppComponent: AfterViewInit");
+    }
+
+    ngAfterViewChecked(){
+      console.log("AppComponent: AfterContentChecked");
+    }
+   
+    ngOnDestroy(){
+      console.log("AppComponent: OnDestroy");
+    }
+  }
 
   title: string = 'Mi nueva Pagina';
 
