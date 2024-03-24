0a33006aef523630325f69b82915893f45eb16a0
Max Rada <xam900mr@gmail.com>
2024-03-23 19:25:48 -0600

    ngIf

--- miAppMovil-1723609/src/app/home/home.module.ts
+++ miAppMovil-1723609/src/app/home/home.module.ts
@@ -5,6 +5,7 @@ import { FormsModule } from '@angular/forms';
 import { HomePage } from './home.page';
 
 import { HomePageRoutingModule } from './home-routing.module';
+import { ListaComponent } from '../lista/lista.component';
 
 
 @NgModule({
@@ -14,6 +15,6 @@ import { HomePageRoutingModule } from './home-routing.module';
     IonicModule,
     HomePageRoutingModule
   ],
-  declarations: [HomePage]
+  declarations: [HomePage, ListaComponent]
 })
 export class HomePageModule {}
